dae56d507c248618e22e678f47fe9f65600a3ee2
Henree001 <okaforhenry779@gmail.com>
2023-09-21 14:07:08 +0100

    corrected function name in task 4

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -12,7 +12,6 @@ def measure_time(n: int, max_delay: int) -> float:
     to run completely.
     """
     start_time = time.perf_counter()
-    print(start_time)
     asyncio.run(wait_n(n, max_delay))
     end_time = time.perf_counter()
     total_time = end_time - start_time
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,7 +6,7 @@ from typing import List
 task_wait_random = __import__("3-tasks").task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """This function returns a sorted list of time of delay(seconds)."""
     return sorted(
         await asyncio.gather(*(task_wait_random(max_delay) for i in range(n)))
